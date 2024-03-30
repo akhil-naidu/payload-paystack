6475c9aa20e4e2cba0945363bc1322a5e89c804d
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-03-30 19:05:53 +0530

    chore: upgraded to v-0.0.4

--- dev/src/plugins/createCustomer.ts
+++ dev/src/plugins/createCustomer.ts
@@ -19,7 +19,6 @@ export const createCustomer: Plugin = (incomingConfig: Config): Config => {
 
   console.log(incomingConfig)
 
-  // @ts-ignore
   const updatedCollection = incomingConfig.collections.map(collection => {
     if (collection.slug === 'users') {
       return {
---
+++ dev/src/plugins/index.ts
@@ -0,0 +1,2 @@
+export { createCustomer } from './createCustomer'
+export type { PluginTypes } from './types'
